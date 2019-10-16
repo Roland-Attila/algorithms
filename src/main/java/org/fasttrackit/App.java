c2bace45c526a9fdd8dea2a63603d6804664cc06
Roland <rolandattila@yahoo.com>
2019-10-16 15:30:25 +0300

    solved name,age and salary(for loop exercises)

---
+++ src/main/java/org/fasttrackit/App.java
@@ -0,0 +1,19 @@
+package org.fasttrackit;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        ForLoop forLoop = new ForLoop();
+        forLoop.forExercise(15);
+        forLoop.anotherForExercise(23);
+        forLoop.anotherForExercise1(220, 500);
+        forLoop.anotherForExercise2(220, 320);
+        forLoop.anotherForExercise3(1, 100);
+        forLoop.anotherForExercise4(1, 100);
+        forLoop.anotherForExercise5(5);
+        forLoop.anotherForExercise6(15);
+        forLoop.anotherForExercise7();
+    }
+}
--- src/main/java/org/fasttrackit/ForLoop.java
+++ src/main/java/org/fasttrackit/ForLoop.java
@@ -66,10 +66,47 @@ public class ForLoop {
         }
     }
 
-//    public double anotherForExercise5(int start) {
-//        for (int i = start; i <= 100; i++) {
-//            System.out.println(i + i);
-//            return double;
-//        }
-//    }
+    public int anotherForExercise5(int start) {
+        int j = 0;
+        for (int i = start; i <= 100; i++) {
+            j = j + i;
+        }
+        System.out.println("\n");
+        System.out.println(j);
+        return j;
+    }
+
+    public double anotherForExercise6(int start) {
+        double k = 5.8;
+        int l = 1;
+        double rezultat = 1;
+        for (int i = start; i <= 100; i++) {
+            k = k + i;
+            l++;
+            rezultat = k / l;
+        }
+        System.out.println("\n");
+        System.out.println(rezultat);
+        return rezultat;
+    }
+
+    public void anotherForExercise7() {
+        System.out.println("\n");
+        for (int i = 1; i <= 7; i++) {
+            if (i <= 1)
+                System.out.println("*******");
+            else if (i <= 2)
+                System.out.println("******");
+            else if (i <= 3)
+                System.out.println("*****" + "\n");
+            else if (i <= 4)
+                System.out.println("****");
+            else if (i <= 5)
+                System.out.println("***");
+            else if (i <= 6)
+                System.out.println("**");
+            else if (i <= 7)
+                System.out.println("*");
+        }
+    }
 }
